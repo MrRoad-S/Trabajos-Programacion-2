816ffb00007b9ffb97a1505840ce644d4c90c3c2
Laboratorio302 <estudiante302@unicamacho.co>
2026-05-12 12:27:44 -0500

    Projecto

---
+++ Segundo Corte/ProjectoFinal/src/test/java/com/DreamlandInventory/ProjectoFinal/ProjectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DreamlandInventory.ProjectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
